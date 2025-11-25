30465697d8aad989a81b3e8355bdc04822969067
Mikhail_Prokhorov <exolated@gmail.com>
2025-11-25 12:23:11 +0300

    test site v.1

---
+++ server.js
@@ -0,0 +1,33 @@
+const fastify = require('fastify')({
+    logger: true
+});
+const path = require('path');
+const fs = require('fs').promises;
+
+fastify.register(require('@fastify/static'), {
+    root: path.join(__dirname, 'public'),
+    prefix: '/'
+});
+
+fastify.get('/', async (request, reply) => {
+    try {
+        const html = await fs.readFile(path.join(__dirname, 'index.html'), 'utf8');
+        reply.type('text/html');
+        return html;
+    } catch (error) {
+        reply.type('text/html');
+        return '<p>HTML file not found</p>';
+    }
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+    console.log('Server is running on http://localhost:3000');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+
+start();
