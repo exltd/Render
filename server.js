6564e1d7df0defc224eb840d6f3cc611099dc9e8
Mikhail_Prokhorov <exolated@gmail.com>
2025-11-26 10:46:38 +0300

    replace port 3000

--- server.js
+++ server.js
@@ -22,8 +22,11 @@ fastify.get('/', async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
-    console.log('Server is running on http://localhost:3000');
+    await fastify.listen({ 
+      port: process.env.PORT || 3000, 
+      host: '0.0.0.0' 
+    })
+    console.log(`Server is running on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
